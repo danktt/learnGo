998ec1619acfbc308b1e5597027fa17e439eb03b
Danilo Miranda <danilomirandaa.15@outlook.com>
2025-10-25 15:22:37 -0400

    res and rem

--- main.go
+++ main.go
@@ -6,6 +6,24 @@ import (
 	"main.go/pacote"
 ) 
 
+
+
 func main() {
 	fmt.Println(pacote.Fus, pacote.Ro, pacote.Dah + "!")
+
+	fmt.Println(soma(2, 4))
+	fmt.Println(dividir(5, 2))
+}
+
+
+
+func soma( a, b  int) int {
+	return a + b 
+}
+
+func dividir ( a, b int) (resultado, resto int ) {
+	resultado = a / b
+	resto = a % b
+
+	return resultado, resto
 }
